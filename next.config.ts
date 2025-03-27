5b017fb282d455c38529d67ea5bd691d73ac1751
zainsci <60652827+zainsci@users.noreply.github.com>
2025-03-27 15:25:01 +0300

    add deploy script

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,11 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from "next"
+const basePath = process.env.NODE_ENV === "development" ? "" : "/eid-card"
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  reactStrictMode: true,
-};
+	/* config options here */
+	reactStrictMode: true,
+	output: "export",
+	basePath,
+}
 
-export default nextConfig;
+export default nextConfig
